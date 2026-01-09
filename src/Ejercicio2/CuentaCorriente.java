61ff6b03d0660f7199e88f81cb11cde473d78325
jgarmej769-aula <jgarmej769@g.educaand.es>
2026-01-09 13:55:50 +0100

    Subida

--- src/Ejercicio2/CuentaCorriente.java
+++ src/Ejercicio2/CuentaCorriente.java
@@ -1,22 +1,29 @@
-package Ejercicio1;
-
-import java.util.Scanner;
+package Ejercicio2;
 
 public class CuentaCorriente {
     String dni;
     String nombreTitular;
     double saldo;
 
-    public CuentaCorriente (String dni, String nombreTitular){
+    public CuentaCorriente (String dni, String nombreTitular, double saldo){
         this.dni = dni;
         this.nombreTitular = nombreTitular;
-        this.saldo = 0;
+        this.saldo = saldo;
     }
+
+    public CuentaCorriente (String dni, String nombreTitular){
+        this(dni, nombreTitular, 0);
+    }
+
+    public CuentaCorriente(String dni, double saldo){
+        this(dni, "",saldo);
+    }
+
     double sacarDinero (double saldoRetirar){
         if (saldoRetirar > this.saldo){
             System.out.println("La operación no se ha podido llevar a cabo debido a que no tiene saldo suficiente");
         }else{
-            this.saldo = this.saldo - saldoRetirar;
+            this.saldo = this.saldo -saldoRetirar;
             System.out.println("Ha retirado "+saldoRetirar+"€, sau saldo acutal es de "+this.saldo+"€.");
         }
         return this.saldo;
@@ -33,6 +40,6 @@ public class CuentaCorriente {
         System.out.println("\n\n=====DATOS DE LA CUENTA CORRIENTE ====");
         System.out.println("Titular de la Cuenta: "+nombreTitular);
         System.out.println("DNI del titular: "+dni);
-        System.out.println("Saldo Actual: "+saldo+"€");
+        System.out.println("Saldo Actual: "+this.saldo+"€");
     }
 }
---
+++ src/Ejercicio2/Main.java
@@ -0,0 +1,13 @@
+package Ejercicio2;
+
+public class Main {
+    public static void main(String[] args){
+        CuentaCorriente sinNombre = new CuentaCorriente("4698615H",15);
+        CuentaCorriente sinSaldo = new CuentaCorriente("48965314H","Mauro");
+        CuentaCorriente cuentaCompleta = new CuentaCorriente("8464654H","NombreCompleto",60);
+
+        sinNombre.mostrarInformacion(); //el nombre debe estar vacío
+        sinSaldo.mostrarInformacion(); //el saldo debe ser 0
+        cuentaCompleta.mostrarInformacion(); //datos completos
+    }
+}
