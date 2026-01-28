4816da037d60edeecb1d27280128bd590de1221e
Juan <jgarmej769@g.educaand.es>
2026-01-28 19:54:46 +0100

    PrimeraParte del ejercicio de la practica 2324 totalmente finalizado

---
+++ src/PracticaEntregable2324Ejercicio1/Main.java
@@ -0,0 +1,52 @@
+package PracticaEntregable2324Ejercicio1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Indique cuantas personas quieres: ");
+        int tam = sc.nextInt();
+        Persona array[] = new Persona[tam];
+        int datosSalida[] = new int[tam];
+        String nombres[] = new String[tam];
+
+        for (int i = 0; i < tam; i++) {
+            System.out.println("Introduzca el nombre de la persona "+i+": ");
+            String nombre = sc.next();
+            nombres[i] = nombre;
+
+            System.out.println("Introduzca su peso: ");
+            double peso = sc.nextDouble();
+
+            System.out.println("Introduzca su edad: ");
+            int edad= sc.nextInt();
+
+            System.out.println("Introduzca su altura: ");
+            double altura = sc.nextDouble();
+
+            System.out.println("Introduzca su sexo (H/M): ");
+            String sexo = sc.next();;
+
+            Persona temporal = new Persona(nombre,edad,"",peso,altura,sexo);
+
+            datosSalida[i] = temporal.calcularMC();
+        }
+
+        System.out.println("\n**************RESULTADO DE LOS CÁLCULOS**************");
+        for (int i = 0; i < tam; i++) {
+            System.out.print(nombres[i]);
+            int dato = datosSalida[i];
+
+            if (dato == 0){
+                System.out.print(": Peso ideal");
+            }else if (dato == -1){
+                System.out.print(": Por debajo de su peso ideal");
+            }else if (dato == 1){
+                System.out.print(": Sobrepeso");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/PracticaEntregable2324Ejercicio1/Persona.java
@@ -0,0 +1,89 @@
+package PracticaEntregable2324Ejercicio1;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String dni;
+    private double peso;
+    private double altura;
+    private String genero;
+
+    public Persona() {
+        this.nombre = "";
+        this.edad = 0;
+        this.dni = "";
+        this.genero = "hombre";
+        this.peso = 0;
+        this.altura = 0;
+    }
+    public Persona(String nombre){
+        this();
+        this.nombre = nombre;
+    }
+    public Persona (String nombre, int edad, String dni, double peso, double altura, String genero){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+        this.genero = genero;
+    }
+
+    int calcularMC(){
+
+        double indiceMasa = this.peso / (this.altura * this.altura);
+        int estado;
+        if ( indiceMasa < 20){
+            estado = -1; //poco peso
+        }else if (indiceMasa >= 20 && indiceMasa <= 25){
+            estado = 0; //peso ideal
+        }else {
+            estado = 1; // sobrepeso
+        }
+        return estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
